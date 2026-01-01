5b158236b5957db0997956aec9af97ae4cea0a3e
Xavier M <xaviermorejonbalta@gmail.com>
2026-01-01 22:58:39 +0000

    The width(543) and height(250) are both fixed numbers,        maybe you

--- src/components/dashboard/NetWorthChart.tsx
+++ src/components/dashboard/NetWorthChart.tsx
@@ -33,7 +33,7 @@ export const NetWorthChart: FC<NetWorthChartProps> = ({ data, translations, loca
       </CardHeader>
       <CardContent>
         <ChartContainer config={chartConfig} className="h-[250px] w-full">
-          <ResponsiveContainer>
+          <ResponsiveContainer width="100%" height="100%">
             <AreaChart data={data} margin={{ top: 5, right: 10, left: -20, bottom: 0 }}>
               <defs>
                 <linearGradient id="fillNetWorth" x1="0" y1="0" x2="0" y2="1">
