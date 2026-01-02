6d54718c222b02aa00cbe2dfdeebd4846671f789
Xavier M <xaviermorejonbalta@gmail.com>
2026-01-02 22:51:08 +0000

    can I already delete the app/src/app folder or not yet?

--- src/components/importer/ImporterClient.tsx
+++ src/components/importer/ImporterClient.tsx
@@ -202,8 +202,7 @@ export function ImporterClient({ banks, debts, assets }: ImporterClientProps) {
                       name="itemId"
                       render={({ field }) => (
                         <FormItem>
-                          <FormLabel>{entryType}</FormLabel>
-                          <Select onValueChange={field.onChange} defaultValue={field.value} value={field.value}>
+                          <FormLabel>{entryType}</FormLabel>                          <Select onValueChange={field.onChange} defaultValue={field.value} value={field.value}>
                             <FormControl>
                               <SelectTrigger>
                                 <SelectValue placeholder={`Select a ${entryType.toLowerCase()}`} />
