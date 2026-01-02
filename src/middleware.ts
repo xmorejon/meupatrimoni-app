fbf063c901445bcf350643822699116c19680af0
Xavier M <xaviermorejonbalta@gmail.com>
2026-01-02 00:13:36 +0000

    Try fixing this error: `Console Error: Unable to find `next-intl` locale

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,5 +6,8 @@ export default createMiddleware({
 });
  
 export const config = {
-  matcher: ['/', '/(ca|es|en)/:path*']
+  // Match all pathnames except for
+  // - … if they start with `/api`, `/_next` or `/_vercel`
+  // - … the ones containing a dot (e.g. `favicon.ico`)
+  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
 };
