7b0217c4fcbdaa59ddba44907fb839bbf5f25c38
xmorejon <xavi@morejon.cat>
2026-01-12 18:08:00 +0100

    Stable Version in VS code

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,7 +1,11 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
-import { getAuth, browserLocalPersistence, setPersistence } from "firebase/auth";
+import {
+  getAuth,
+  browserLocalPersistence,
+  setPersistence,
+} from "firebase/auth";
 import { getFunctions, httpsCallable } from "firebase/functions"; // Import the functions module
 
 // Your web app's Firebase configuration
@@ -11,7 +15,7 @@ const firebaseConfig = {
   projectId: "meupatrimoni-app",
   storageBucket: "meupatrimoni-app.firebasestorage.app",
   messagingSenderId: "792203456948",
-  appId: "1:792203456948:web:3b76693471fbc59ba6c2e1"
+  appId: "1:792203456948:web:3b76693471fbc59ba6c2e1",
 };
 
 // Initialize Firebase
@@ -23,12 +27,11 @@ const auth = getAuth(app);
 setPersistence(auth, browserLocalPersistence);
 
 // Initialize Cloud Functions and point them to the correct region
-const functions = getFunctions(app, 'europe-west1');
+const functions = getFunctions(app, "europe-west1");
 
-const getAuthLink = httpsCallable(functions, 'getTrueLayerAuthLink');
-const handleCallback = httpsCallable(functions, 'handleTrueLayerCallback');
-const importCsv = httpsCallable(functions, 'importCsv');
-
+const getAuthLink = httpsCallable(functions, "getTrueLayerAuthLink");
+const handleCallback = httpsCallable(functions, "handleTrueLayerCallback");
+const importCsv = httpsCallable(functions, "importCsv");
 
 // Export all the firebase services
 export { app, db, auth, functions, getAuthLink, handleCallback, importCsv };
